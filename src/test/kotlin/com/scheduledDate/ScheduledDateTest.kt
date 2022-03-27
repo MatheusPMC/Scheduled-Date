1b2f3f759b5043ba5593e7dcfb973bb449578238
MatheusPMC <matheus.pmc@hotmail.com>
2022-03-27 14:05:21 -0300

    Init

---
+++ src/test/kotlin/com/scheduledDate/ScheduledDateTest.kt
@@ -0,0 +1,12 @@
+package com.scheduledDate
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.kotest.annotation.MicronautTest
+import io.kotest.core.spec.style.StringSpec
+
+@MicronautTest
+class ScheduledDateTest(private val application: EmbeddedApplication<*>): StringSpec({
+
+    "test the server is running" {
+        assert(application.isRunning)
+    }
+})
